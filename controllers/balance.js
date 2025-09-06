48e9e78756057ea392372c9d2203ee6cff2b7ac6
salim <salimafzal.shaikh@gmail.com>
2025-09-06 12:52:29 +0530

    add console

--- controllers/balance.js
+++ controllers/balance.js
@@ -5,6 +5,8 @@ const { sendError, sendSuccess } = require("../Middleware/response");
 exports.createBalance = async (req, res) => {
   try {
       // const balance = await Balance.create(req.body);
+      console.log('test');
+      
       res.status(201).json('Test');
   } catch (err) {
     res.status(500).json({ error: err.message });
